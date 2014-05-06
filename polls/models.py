df0c014c4cd1bf56e1aa2a3c8ef9adb51ee868af
Tom Debruyne <tomdebruyne@gmail.com>
2014-05-06 14:41:21 +0200

    typo fix

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,7 @@ class Poll(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date => timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question
