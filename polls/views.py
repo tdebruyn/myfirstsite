bc1157f262e3eeaf90535de1cf6137d445bc220a
Tom Debruyne <tomdebruyne@gmail.com>
2014-05-06 14:26:40 +0200

    Drop bogus views and create models

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
 
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
 
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
