67b381f027dda0452c2bb16f3a960ac17ece24f0
Tom Debruyne <tomdebruyne@gmail.com>
2014-05-06 11:19:47 +0200

    Modify Database parameters

--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -57,8 +57,11 @@ WSGI_APPLICATION = 'myfirstsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tdebruyn$default ',
+        'USER': 'tdebruyn',
+        'PASSWORD': 'casios',
+        'HOST': 'mysql.server'
     }
 }
 
